7513e8c53f92b8f318ad461286bfcf97eaba36a4
George Gonçalves <georgeggmiranda@gmail.com>
2026-01-25 15:36:48 -0300

    Scroll da letra 'A'

--- scrollMatriz.ino
+++ scrollMatriz.ino
@@ -29,11 +29,39 @@ const byte letraA[8] = {
   0b00
 };
 
-byte colunaA = 0; 
+byte bufferA[8] = {0};
 
 void drawA() {
   for (byte i = 0; i < 8; i++) {
     sendMAX(i + 1, letraA[i]);
+  }
+}
+
+void drawBufferA() {
+  for (byte i = 0; i < 8; i++) {
+    sendMAX(i + 1, bufferA[i]);
+  }
+}
+
+void scrollLeft() {
+  for (byte i = 0; i < 8; i++) {
+    bufferA[i] <<= 1;
+  }
+}
+
+void scrollInLetter(const byte letter[8]) {
+  for (int col = 7; col >= 0; col--) {   // entra coluna por coluna
+    for (byte row = 0; row < 8; row++) {
+      bufferA[row] <<= 1;                // desloca tudo para esquerda
+
+      // pega o bit correto da letra
+      if (letter[row] & (1 << col)) {
+        bufferA[row] |= 1;               // acende bit da direita
+      }
+    }
+
+    drawBufferA();
+    delay(200);
   }
 }
 
@@ -62,7 +90,7 @@ void drawSmile() {
 
 
 // ================================
-// Mostrar "smile1" na matriz
+// Mostrar "coração" na matriz
 // ================================
 const byte coracao[8] = {
   0xFF,
@@ -93,18 +121,18 @@ void clearMatriz() {
 };
 
 void setup() {
-  pinMode (CS_MAX, OUTPUT);
+  pinMode(CS_MAX, OUTPUT);
   digitalWrite(CS_MAX, HIGH);
 
   SPI.begin();
-  SPI.beginTransaction(SPISettings(100000, MSBFIRST,SPI_MODE0));
+  SPI.beginTransaction(SPISettings(100000, MSBFIRST, SPI_MODE0));
 
   // Inicialização do MAX7219
-  sendMAX(0x0C, 0x01); // sai do shutdown
-  sendMAX(0x09, 0x00); // decode OFF (modo matriz)
-  sendMAX(0x0B, 0x07); // 8 linhas
-  sendMAX(0x0A, 0x08); // brilho médio
-  sendMAX(0x0F, 0x00); // display test OFF
+  sendMAX(0x0C, 0x01);  // sai do shutdown
+  sendMAX(0x09, 0x00);  // decode OFF (modo matriz)
+  sendMAX(0x0B, 0x07);  // 8 linhas
+  sendMAX(0x0A, 0x08);  // brilho médio
+  sendMAX(0x0F, 0x00);  // display test OFF
 
   clearMatriz();
 }
@@ -113,25 +141,15 @@ void setup() {
 // LOOP
 // ================================
 void loop() {
-  // Mostrando a letra "A" com um delay de 1000 milessegundos
-  drawA();
-  delay(1000);
-
-  //Mostrando o "smile" com delay de 1000 milessegundos
   drawSmile();
   delay(1000);
 
-  // Mostrando o "coração" com delay de 1000 milessegundos
+  drawA();
+  delay(3000);
+
   drawCoracao();
-  delay(10000);
+  delay(5000);
 
-  // Acende todos os LEDs e faz parecido com uma respiração
-  for (int i = 1; i < 8 ; i++) {
-    sendMAX(i, 0xFF);
-  }  
-
-  for (int i = 0; i < 15; i++) {
-    sendMAX(0X0A, i);
-    delay(100);
-  }
+  scrollInLetter(letraA);
+  delay(1000);
 }
